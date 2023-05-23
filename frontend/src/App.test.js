945dd27190acbe19c8d034bfeefb9710f212199e
ArunabhPrakash <95112140+ArunabhPrakash@users.noreply.github.com>
2023-05-23 08:14:42 +0530

    Update App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react link', () => { //used to define func in js
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
